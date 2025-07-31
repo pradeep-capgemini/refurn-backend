3550ca144ec27fef05c0981e351974be17d2ed2c
Kumar <pradeep.n.kumar@capgemini.com>
2025-07-31 13:15:34 +0530

    add product, selling history,admin approval api done

---
+++ src/main/java/com/capgemini/entity/Product.java
@@ -0,0 +1,97 @@
+package com.capgemini.entity;
+
+import java.util.Date;
+
+import com.capgemini.enums.ProductStatus;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+    private String category;
+    private String description;
+    private byte[] productImg;
+    private Double price;
+    private Double discount;
+    private String productSellerName;
+    private ProductStatus productStatus;
+    private Date createdAt;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public Double getDiscount() {
+		return discount;
+	}
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+	public String getProductSellerName() {
+		return productSellerName;
+	}
+	public void setProductSellerName(String productSellerName) {
+		this.productSellerName = productSellerName;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public ProductStatus getProductStatus() {
+		return productStatus;
+	}
+	public void setProductStatus(ProductStatus productStatus) {
+		this.productStatus = productStatus;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public byte[] getProductImg() {
+		return productImg;
+	}
+	public void setProductImg(byte[] productImg) {
+		this.productImg = productImg;
+	}
+	
+	
+
+    
+}
+
